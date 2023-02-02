7496657213e97b29307241cb201ff718d93071be
shakh <user@WIN-HFVP9OKK195>
2023-02-02 16:28:34 +0300

    JWT Token

---
+++ JWTTokenAuth/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTTokenAuth.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class UsersController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetUsers()
+    {
+        return Ok(User.FindFirst(ClaimTypes.Name)?.Value);
+    }
+
+    [HttpPost]
+    [AllowAnonymous]
+    public IActionResult LoginUser(string username)
+    {
+        var keyByte = System.Text.Encoding.UTF8.GetBytes("asda;odbuads;b242342hbiahbasdada");
+        var securityKey = new SigningCredentials(new SymmetricSecurityKey(keyByte), SecurityAlgorithms.HmacSha256);
+
+        var security = new JwtSecurityToken(
+            issuer:"Project1",
+            audience:"Rooom1",
+            new Claim[]
+            {
+                new Claim(ClaimTypes.Name, username)
+            },
+            expires: DateTime.Now.AddSeconds(10),
+            signingCredentials: securityKey);
+
+        var token = new JwtSecurityTokenHandler().WriteToken(security);
+
+        return Ok(token);
+    }
+}
---
+++ JWTTokenAuth/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(jwtoption =>
+    {
+        jwtoption.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidIssuer = "Project1",
+            ValidateAudience = true,
+            ValidAudience = "Rooom1",
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero,
+            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("asda;odbuads;b242342hbiahbasdada"))
+        };
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
